6f4b9e3546f28d4fc1ce58b46f5285df1b72a99d
Shawn Ensz <shawn.ensz@daktronics.com>
2021-08-31 20:04:45 -0500

    Form resizing/centering and Added label with some formatting

--- P1 Code/P1 Code/FormHelloWorld.Designer.cs
+++ P1 Code/P1 Code/FormHelloWorld.Designer.cs
@@ -29,19 +29,37 @@ namespace P1_Code
         /// </summary>
         private void InitializeComponent()
         {
+            this.label1 = new System.Windows.Forms.Label();
             this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 48F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(40, 80);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(448, 91);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Hello World";
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(532, 353);
+            this.Controls.Add(this.label1);
             this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "CSC 470 - P1";
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
+
+        private System.Windows.Forms.Label label1;
     }
 }
