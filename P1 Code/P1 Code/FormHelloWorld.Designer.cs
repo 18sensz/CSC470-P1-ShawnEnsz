51d7437c218848d8e7a45f589fcf133aae5ecfe1
Shawn Ensz <shawn.ensz@daktronics.com>
2021-08-31 20:29:46 -0500

    Added Hello World Label and formatted it

--- P1 Code/P1 Code/FormHelloWorld.Designer.cs
+++ P1 Code/P1 Code/FormHelloWorld.Designer.cs
@@ -29,20 +29,36 @@ namespace P1_Code
         /// </summary>
         private void InitializeComponent()
         {
+            this.label1 = new System.Windows.Forms.Label();
             this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 40.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(70, 40);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(387, 78);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Hello World";
+            this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
             // 
             // FormHelloWorld
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(532, 253);
+            this.Controls.Add(this.label1);
             this.Name = "FormHelloWorld";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "CSC 470 - P1";
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
+
+        private System.Windows.Forms.Label label1;
     }
 }
